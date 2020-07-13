f35f75fa2609a1dcfd639d8cabf0879ce8f8e5c9
MengJianchao <jianchaomeng@sohu-inc.com>
2020-07-13 23:23:27 +0800

    test

--- src/mjc/leetcode/Q350.java
+++ src/mjc/leetcode/Q350.java
@@ -3,7 +3,7 @@ package mjc.leetcode;
 public class Q350 {
     public static void main(String[] args) {
         Q350 q350 = new Q350();
-        q350.intersect(new int[]{1,2,3}, new int[]{2,3,4})
+        q350.intersect(new int[]{1,2,3}, new int[]{2,3,4});
     }
 
     public int[] intersect(int[] nums1, int[] nums2) {
